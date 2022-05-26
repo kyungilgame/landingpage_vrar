a454e9f20af0426b4efacaa08ba3dbd090ce547f
KIMGEON88 <geonking88@gmail.com>
2022-05-26 10:39:37 +0900

    modifed

--- src/Component/MobileBottomCTABar.js
+++ src/Component/MobileBottomCTABar.js
@@ -76,7 +76,7 @@ const XImageRight = styled.div`
   transform: rotate(-45deg);
 `;
 
-const dday = new Date("May 23, 2022, 0:00:00").getTime();
+const dday = new Date("June 3, 2022, 0:00:00").getTime();
 
 const today = new Date().getTime(); // 밀리 초 단위
 const gap = dday - today;
